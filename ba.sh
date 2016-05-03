10c74832b4fd6798a224bf13d28845cb417183da
AymanYac <yac.ayman@hotmail.com>
2016-05-03 12:14:03 +0100

    Bash for TEMPLATE

---
+++ ba.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo mkdir -p /usr/local/share/texmf/tex/latex/rustyle/latex_beamer_ru
+sudo unzip ~/Downloads/latex_beamer_ru.zip -d /usr/local/share/texmf/tex/latex/rustyle/latex_beamer_ru
+sudo chmod -R a+rX /usr/local/share/texmf/tex/latex/rustyle
+sudo texhash
+URL="https://www.sharelatex.com/templates/536e933280bde19571bf9c33/v/1/zip?name=Radboud%20University%20Beamer%20(version%201)"
+wget $URL -O Radboud\ University\ Beamer\ \(version\ 1\).zip
+unzip Radboud\ University\ Beamer\ \(version\ 1\).zip -d .
+rm Radboud\ University\ Beamer\ \(version\ 1\).zip
